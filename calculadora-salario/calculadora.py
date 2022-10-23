680c97b93604e6949b0ccba50999ad0a6dff67c3
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2022-10-23 13:50:55 -0300

    Adicionar feature para dba

--- calculadora-salario/calculadora.py
+++ calculadora-salario/calculadora.py
@@ -1,3 +1,5 @@
 def calcula_salario(cargo, salario):
     if cargo == 'desenvolvedor':
         return salario * .9 if salario < 3000 else salario * .8
+    if cargo == 'dba':
+        return salario * .85 if salario < 2000 else salario * .75
