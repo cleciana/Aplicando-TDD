58341d7f5c6c4e22f9a1051ecf3e0f55f60aeb16
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2022-10-23 13:23:19 -0300

    Adicionando esqueleto para testes

---
+++ calculadora-salario/test_tdd.py
@@ -0,0 +1,13 @@
+from .code import calcula_salario
+
+def test_salario_dev():
+    pass
+
+def test_salario_dba():
+    pass
+
+def test_salario_testador():
+    pass
+
+def test_salario_gerente():
+    pass
