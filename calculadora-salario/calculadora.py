227fabd96005dd5ebe78d1f2910d858343da4538
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2022-10-23 13:53:26 -0300

    Adicionar feature para gerente

--- calculadora-salario/calculadora.py
+++ calculadora-salario/calculadora.py
@@ -3,4 +3,6 @@ def calcula_salario(cargo, salario):
         return salario * .9 if salario < 3000 else salario * .8
     if cargo in ['dba', 'testador']:
         return salario * .85 if salario < 2000 else salario * .75
+    if cargo == 'gerente':
+        return salario * .8 if salario < 5000 else salario * .7
     
