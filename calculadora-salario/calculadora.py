cbba2c6f2310b4b4ffd479e28f52b7714c56b0d6
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2022-10-23 13:51:25 -0300

    Adicionar feature para testador

--- calculadora-salario/calculadora.py
+++ calculadora-salario/calculadora.py
@@ -3,3 +3,5 @@ def calcula_salario(cargo, salario):
         return salario * .9 if salario < 3000 else salario * .8
     if cargo == 'dba':
         return salario * .85 if salario < 2000 else salario * .75
+    if cargo == 'testador':
+        return salario * .85 if salario < 2000 else salario * .75
