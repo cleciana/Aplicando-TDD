efee3e31993883ec6d26ea07fa6520bd666aad17
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2022-10-23 13:52:07 -0300

    Removendo duplicacao de codigo

--- calculadora-salario/calculadora.py
+++ calculadora-salario/calculadora.py
@@ -1,7 +1,6 @@
 def calcula_salario(cargo, salario):
     if cargo == 'desenvolvedor':
         return salario * .9 if salario < 3000 else salario * .8
-    if cargo == 'dba':
+    if cargo in ['dba', 'testador']:
         return salario * .85 if salario < 2000 else salario * .75
-    if cargo == 'testador':
-        return salario * .85 if salario < 2000 else salario * .75
+    
