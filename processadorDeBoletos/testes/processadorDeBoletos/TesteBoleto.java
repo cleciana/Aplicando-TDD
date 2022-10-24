98184176579212fad312480b9182dea74ca27325
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2022-10-24 12:55:26 -0300

    conf inicial

---
+++ processadorDeBoletos/testes/processadorDeBoletos/TesteBoleto.java
@@ -0,0 +1,14 @@
+package processadorDeBoletos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteBoleto {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
