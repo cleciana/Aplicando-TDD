8531888c28d44eca0c5da5745e5dd3d4435717ea
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2022-10-24 14:14:31 -0300

    teste

---
+++ processadorDeBoletos/testes/processadorDeBoletos/TesteFatura.java
@@ -0,0 +1,33 @@
+package processadorDeBoletos;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteFatura {
+	
+	Fatura fatura1;
+	Boleto boleto1, boleto2;
+	ArrayList<Boleto> boletos1;
+	
+	@Before
+	public void before() {
+		fatura1 = new Fatura(LocalDate.now(), 1500.00, "João");
+		boleto1 = new Boleto(LocalDate.now(), 700.00);
+		boleto2 = new Boleto(LocalDate.now(), 800.00);
+		boletos1 = new ArrayList<Boleto>();
+		boletos1.add(boleto1);
+		boletos1.add(boleto2);
+	}
+	
+	@Test
+	public void testPagamentoTotal() {
+		fatura2.processaPagamentos(boletos2);
+		assertEquals(0.00, fatura2.getValorTotal());
+	}
+	
+}
